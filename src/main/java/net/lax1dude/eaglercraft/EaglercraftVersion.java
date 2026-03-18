06bd1bbeab7360c5647982d785de6ca132e0eab3
arlenh7 <arlenjou336@gmail.com>
2026-03-18 10:24:54 -0400

    whoops

--- src/main/java/net/lax1dude/eaglercraft/EaglercraftVersion.java
+++ src/main/java/net/lax1dude/eaglercraft/EaglercraftVersion.java
@@ -2,7 +2,7 @@ package net.lax1dude.eaglercraft;
 
 public class EaglercraftVersion {
 	
-	public static final String localStorageNamespace = "_eaglercraft_1.0.0";
+	public static final String localStorageNamespace = "_eaglercraft_1.3.2";
 	public static final String versionName = "25w27a";
 
 }
